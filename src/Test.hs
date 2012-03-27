6efec07865cfa17972f7fc0378fc1e6f1aed434b
Mahmut Bulut <mahmutbulut0@gmail.com>
2012-03-27 21:08:26 +0300

    Initial commit

---
+++ src/Test.hs
@@ -0,0 +1,20 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Main
+-- Copyright   :  2012 (c) Mahmut Bulut
+-- License     :  AllRightsReserved
+--
+-- Maintainer  :  Mahmut Bulut
+-- Stability   :  unstable
+-- Portability :  
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module Main (
+    main
+) where
+
+main = putStrLn "Put your test code here"
+
